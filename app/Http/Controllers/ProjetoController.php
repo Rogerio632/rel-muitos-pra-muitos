af7ecb582f4a90ea718dfc780df87a3f6c6821fb
Rogerio632 <rogeriofilho632@gmail.com>
2019-03-12 17:33:32 -0300

    Adição de Condicional para dev

--- app/Http/Controllers/ProjetoController.php
+++ app/Http/Controllers/ProjetoController.php
@@ -22,14 +22,16 @@ class ProjetoController extends Controller
         echo "<ul><u>" . $p->nome . " | Carga horária estimada: " . $p->estimativa_hora . " horas</u></ul>";
         
         echo "<p><b>Equipe:</b><br> ";
-            
+        if(count($p->desenvolvedores) > 0){  
         foreach($p->desenvolvedores as $d){
          
              echo "~" . $d->nome . " | " .
             "Cargo: " . $d->area .  
             " | Carga Horária Semanal: " . $d->pivot->horas_semanais . " horas<br>" ;
             
-            }
+            }} else{
+            echo "Não há nenhum desenvolvedor alocado neste projeto.";
+        }
               echo "<hr>";      
 
         }
